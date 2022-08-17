6766a6674550726f786162e154f50d09964520b6
Aid Death <aiddeath@gmail.com>
2022-08-17 10:18:52 +0300

    Another ViewModelLocator sample, design-time autocomplete added

---
+++ ViewModels/ViewModelLocator2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Globalization;
+using System.Reflection;
+using WpfMvvmDiEfSample.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfMvvmDiEfSample.ViewModels
+{
+    public static class ViewModelLocator2
+    {
+        public static DependencyProperty AutoWireViewModelProperty = DependencyProperty.RegisterAttached("AutoWireViewModel", typeof(bool),
+            typeof(ViewModelLocator), new PropertyMetadata(false, AutoWireViewModelChanged));
+
+        public static bool GetAutoWireViewModel(UIElement element)
+        {
+            return (bool)element.GetValue(AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(UIElement element, bool value)
+        {
+            element.SetValue(AutoWireViewModelProperty, value);
+        }
+
+        private static void AutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+                Bind(d);
+        }
+
+        private static void Bind(DependencyObject view)
+        {
+            if (view is FrameworkElement frameworkElement)
+            {
+                var viewModelType = FindViewModel(frameworkElement.GetType());
+                //frameworkElement.DataContext = Activator.CreateInstance(viewModelType);
+                frameworkElement.DataContext = ActivatorUtilities.CreateInstance(App.Current.Services, viewModelType);
+            }
+        }
+
+        private static Type FindViewModel(Type viewType)
+        {
+            string viewName = string.Empty;
+
+            if (viewType.FullName.EndsWith("Window"))
+            {
+                viewName = viewType.FullName
+                    .Replace("Views", "ViewModels");
+            }
+
+            var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+            var viewModelName = string.Format(CultureInfo.InvariantCulture, "{0}ViewModel, {1}", viewName, viewAssemblyName);
+
+            return Type.GetType(viewModelName);
+        }
+    }
+}
